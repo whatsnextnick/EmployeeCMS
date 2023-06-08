10a2357a879a866589470b7452e25f0f18cbc954
Jawascript <n.jmonplaisir@gmail.com>
2023-06-08 14:40:03 -0400

    adding 'view' functions and updateEmployees

--- cms/cms.js
+++ cms/cms.js
@@ -34,3 +34,59 @@ function addDepartment() {
       });
     });
 }
+
+// Function to view departments
+function viewDepartments() {
+  pool.query('SELECT * FROM department', (error, results) => {
+    if (error) throw error;
+    console.table(results);
+    mainMenu();
+  });
+}
+
+// Function to view roles
+function viewRoles() {
+  pool.query('SELECT * FROM role', (error, results) => {
+    if (error) throw error;
+    console.table(results);
+    mainMenu();
+  });
+}
+
+// Function to view employees
+function viewEmployees() {
+  pool.query('SELECT * FROM employee', (error, results) => {
+    if (error) throw error;
+    console.table(results);
+    mainMenu();
+  });
+}
+
+// Function to update employee roles
+function updateEmployee() {
+  inquirer
+    .prompt([
+      {
+        name: 'employee_id',
+        type: 'input',
+        message: 'Enter the employee ID:',
+      },
+      {
+        name: 'new_role_id',
+        type: 'input',
+        message: 'Enter the new role ID:',
+      },
+    ])
+    .then((answers) => {
+      const { employee_id, new_role_id } = answers;
+      pool.query(
+        'UPDATE employee SET role_id = ? WHERE id = ?',
+        [new_role_id, employee_id],
+        (error) => {
+          if (error) throw error;
+          console.log('Employee role updated successfully.');
+          mainMenu();
+        }
+      );
+    });
+}

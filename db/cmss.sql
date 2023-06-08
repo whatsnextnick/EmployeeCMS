6826664b9a49cc4c99648f53b9643bb1722645ae
Jawascript <n.jmonplaisir@gmail.com>
2023-06-08 14:37:35 -0400

    adding employee table to .sql

--- db/cmss.sql
+++ db/cmss.sql
@@ -14,3 +14,13 @@ CREATE TABLE role (
   department_id INT,
   FOREIGN KEY (department_id) REFERENCES department(id)
 );
+
+CREATE TABLE employee (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT,
+  manager_id INT,
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
